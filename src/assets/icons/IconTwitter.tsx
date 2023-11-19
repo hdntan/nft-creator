a15b2a5c4ec0782201f168d57ae426749f62fa53
hdntan <hdntan0903@gmail.com>
2023-11-19 12:23:13 +0700

    fix: fix component icon

---
+++ src/assets/icons/IconTwitter.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+
+export interface IIconTwitterProps {}
+
+export default function IconTwitter(props: IIconTwitterProps) {
+  return (
+    <svg
+      width="18"
+      height="16"
+      viewBox="0 0 18 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M0 0.013916H1.95122L13.6585 15.6237H11.7073L0 0.013916ZM3.5122 0.013916H5.46341L17.1707 15.6237H15.2195L3.5122 0.013916ZM1.56098 0.013916H5.46341V1.57489H1.56098V0.013916ZM11.7073 14.0627H15.6098V15.6237H11.7073V14.0627ZM13.6585 0.013916H16.3902L3.12195 15.6237H0.390244L13.6585 0.013916Z"
+        fill="#121949"
+      />
+    </svg>
+  );
+}
