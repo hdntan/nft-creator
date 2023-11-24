14108f368a544464f6c4005e4c324c46f95d4f57
hdntan <hdntan0903@gmail.com>
2023-11-24 21:03:15 +0700

    feat: add func toWei, toEth and show toast

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,4 +1,29 @@
+import { ethers } from "ethers";
+
 const imageNameToUrl = (name: string) =>
   `${process.env.NEXT_PUBLIC_API_HOST}/${name}`;
 
-export { imageNameToUrl };
+  import { ToastContainer, toast } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
+   const showErrorToast = (message:any) => {
+      toast.error(message)
+    }
+    
+   const showSuccessToast = (message:any) => {
+      toast.success(message)
+    }
+
+
+
+export function toWei(amount:any, decimal = 6) {
+    const toWei = ethers.utils.parseUnits(amount,decimal);
+    return toWei.toString();
+}
+
+export function toEth(amount:any, decimal = 6) {
+    const toEth = ethers.utils.formatUnits(amount, decimal);
+    return toEth.toString();
+}
+
+
+export { imageNameToUrl, showErrorToast, showSuccessToast };
